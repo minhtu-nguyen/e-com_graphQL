cc80ed2bfb87f11d87a4a44bfc348df40d6780fa
minhtu-nguyen <minhtu-nguyen@outlook.com>
2023-04-17 23:13:40 +0700

    Set up Hero

---
+++ src/components/Hero/index.js
@@ -0,0 +1,16 @@
+import './index.css';
+
+const Hero = () => {
+	return (
+		<div className="hero-container">
+			<h1>
+				Welcome<span>.</span>
+			</h1>
+			<h3>No. 1 Sneakers Online Store</h3>
+			<small>PRODUCT 1 | PRODUCT 2 | PRODUCT 3</small>
+			<a href="#" className="btn">Shop Now</a>
+		</div>
+	);
+};
+
+export default Hero;

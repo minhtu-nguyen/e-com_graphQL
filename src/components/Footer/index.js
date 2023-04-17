5c52d3d294e52e394965c7ad18dd81bffadedb3e
minhtu-nguyen <minhtu-nguyen@outlook.com>
2023-04-17 23:33:23 +0700

    Set up Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import './index.css';
+
+const index = () => {
+	return (
+		<div>
+			<div className="footer">
+				<div className="container footer-flex">
+					<p>&copy; Showcase application built by Minh Tu</p>
+					<p>GitHub</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default index;

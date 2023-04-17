bc00a22e2269c37bdd199fe1a61610966e347369
minhtu-nguyen <minhtu-nguyen@outlook.com>
2023-04-17 23:42:19 +0700

    Set up Pages

---
+++ src/pages/Shop.js
@@ -0,0 +1,60 @@
+import nike1 from './../assets/images/nike-1.jpg';
+import nike2 from './../assets/images/nike-2.jpg';
+import nike3 from './../assets/images/nike-3.jpg';
+
+const Shop = () => {
+	return (
+		<div className="container">
+			<button className="back-btn btn">Back</button>
+			<div className="page-title">
+				<h2>
+					Shop<span>.</span>
+				</h2>
+			</div>
+			<div className="products">
+				<div className="product">
+					<img src={nike1} className="product-img" alt="" /> @todo change these
+					<div className="product-content">
+						<div className="flex-row">
+							<h3>NIKE AIRFORCE !</h3>
+							<p className="price">$512</p>
+						</div>
+						<button className=" btn">Add to Cart</button>
+					</div>
+				</div>
+				<div className="product">
+					<img src={nike2} className="product-img" alt="" />
+					<div className="product-content">
+						<div className="flex-row">
+							<h3>NIKE AIRFORCE !</h3>
+							<p className="price">$512</p>
+						</div>
+						<button className=" btn">Add to Cart</button>
+					</div>
+				</div>
+				<div className="product">
+					<img src={nike1} className="product-img" alt="" />
+					<div className="product-content">
+						<div className="flex-row">
+							<h3>NIKE AIRFORCE !</h3>
+							<p className="price">$512</p>
+						</div>
+						<button className=" btn">Add to Cart</button>
+					</div>
+				</div>
+				<div className="product">
+					<img src={nike3} className="product-img" alt="" />
+					<div className="product-content">
+						<div className="flex-row">
+							<h3>NIKE AIRFORCE !</h3>
+							<p className="price">$512</p>
+						</div>
+						<button className=" btn">Add to Cart</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Shop;
